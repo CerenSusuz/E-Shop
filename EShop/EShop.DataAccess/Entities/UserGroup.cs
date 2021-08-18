ce46823b5d90fd41b97c19365203a78f937818df
Ceren Susuz <ceren199704@hotmail.com>
2021-08-18 16:09:13 +0300

    create entities

---
+++ EShop/EShop.DataAccess/Entities/UserGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EShop.Core.Signatures;
+
+namespace EShop.DataAccess.Entities
+{
+    public class UserGroup : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsBlocked { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CreatedUser { get; set; }
+        public string UpdatedUser { get; set; }
+    }
+}
