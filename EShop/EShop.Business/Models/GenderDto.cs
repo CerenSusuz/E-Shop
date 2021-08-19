d4d50bb998ff253b5db95853b9469a8bbff1c29f
Ceren Susuz <ceren199704@hotmail.com>
2021-08-19 13:52:29 +0300

    .

---
+++ EShop/EShop.Business/Models/GenderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EShop.Business.Models
+{
+    public class GenderDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsBlocked { get; set; }
+    }
+}
